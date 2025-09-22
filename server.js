a24a53163d89229226d0af16fc3547a28747a5bb
TTQBqueen <ttboler@uncg.edu>
2025-09-21 20:34:31 -0400

    Added CORS

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 // Budget API
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 
-
+app.use(cors());
+//If i dont have then my localhost 3000 will not work or has cannot /get
 app.use('/', express.static('public'));
 
 const budget = require('./budget.json');
 
 
-app.get('/hello', (req, res) => {
-    res.send('Welcome to the Budget API');
-});
+// app.get('/hello', (req, res) => {
+//     res.send('Welcome to the Budget API');
+// });
 
 app.get('/budget', (req, res) => {
     res.json(budget);
